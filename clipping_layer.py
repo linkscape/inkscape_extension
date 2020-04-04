4cfddebb1f2eb2519ee9979697b2aee357b7cdf8
linkscape <63139855+linkscape@users.noreply.github.com>
2020-04-04 15:52:41 +0900

    Update clipping_layer.py

--- clipping_layer.py
+++ clipping_layer.py
@@ -5,7 +5,7 @@ This extension Create a clipping layer
 Copyright (C) 2019 Linkscape
 (this is the modified program of the EraserLayer,
 Copyright (C) 2012 Jabiertxo Arraiza, jabier.arraiza@marker.es)
---4/4 first
+--4/4 first!!
 
 
 \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
